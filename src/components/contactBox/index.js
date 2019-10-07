3b314198804a8a7546fcc3ecda73e0ed8f97394b
Elvis <elvis.graholskis@valtech-mobility.com>
2019-10-07 09:14:08 +0200

    first commit

---
+++ src/components/contactBox/index.js
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+import style from './style';
+
+const ContactBox = () => (
+  <section class={"contact-box"}>
+
+    <h4>Contact</h4>
+  </section>
+);
+
+export default ContactBox;
