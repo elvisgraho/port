43594e8bb78ab9131c3447c6f3553a854b4877fa
Elvis <elvis.graholskis@valtech-mobility.com>
2019-10-07 09:35:13 +0200

    Progress on contact box

--- src/components/contactBox/index.js
+++ src/components/contactBox/index.js
@@ -1,11 +1,36 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import style from './style';
 
-const ContactBox = () => (
-  <section class={"contact-box"}>
 
-    <h4>Contact</h4>
-  </section>
-);
+export default class ContactBox extends Component {
 
-export default ContactBox;
+  constructor() {
+    super();
+    this.state = {
+      visible: false
+    }
+  }
+
+  toggleVisibility = e => {
+    this.setState((state) => ({ visible: !state.visible }))
+  };
+
+  getStateClass = () => {
+    return this.state.visible ? " visible" : " hidden";
+  }
+
+  render() {
+    return (
+      <section class={"contact-box"}>
+
+        <div class={"contact-box__info" + this.getStateClass()}>
+          Here is the full information
+        </div>
+
+        <div class={"contact-box__heading"} onClick={this.toggleVisibility}>
+          <h4>Contact</h4>
+        </div>
+      </section>
+    );
+  }
+}
