752af2f57c47de89f75fafa9703ec670ae1c3a9f
Elvis <elvis.graholskis@valtech-mobility.com>
2019-10-08 12:36:37 +0200

    anchor animatiom

--- src/components/skillCard/index.js
+++ src/components/skillCard/index.js
@@ -7,25 +7,35 @@ import * as data from '../../assets/content/general';
 export default class SkillCard extends Component {
 
   state = {
-    swing: false
+    swing: undefined,
+    swingCount: 0
   }
 
   onClick = () => {
     if (!this.state.swing) {
-      this.setState((state) => ({ swing: true }));
 
-      setTimeout(() => {
-        this.setState((state) => ({ swing: false }));
-      }, 1000);
+      if (this.state.swingCount > 0 && this.state.swingCount % 3 === 0) {
+        this.setState((state) => ({ swing: "swing--big", swingCount: this.state.swingCount + 1 }));
+
+        setTimeout(() => {
+          this.setState((state) => ({ swing: undefined }));
+        }, 1000);
+      } else {
+        this.setState((state) => ({ swing: "swing", swingCount: this.state.swingCount + 1 }));
+
+        setTimeout(() => {
+          this.setState((state) => ({ swing: undefined }));
+        }, 1000);
+      }
 
     }
   }
 
   render() {
-    console.log(this.state);
+    console.log(this.state)
     return (
       <section class={"skill-card"}>
-        <Anchor size={22} onClick={this.onClick} class={this.state.swing ? "swing" : ""} />
+        <Anchor size={22} onClick={this.onClick} class={this.state.swing ? this.state.swing : ""} />
         <h2 class="skill-card-title">{this.props.title}</h2>
         <p class="skill-card-text">{this.props.text}</p>
         <div class="skill-card__skills">
